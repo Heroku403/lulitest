1691d0d6c629b6c8932fbb6cb007e49484c4e648
Heroku403 <140687082+Heroku403@users.noreply.github.com>
2024-11-22 13:43:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(level=logging.INFO)
 app = FastAPI()
 
 # Telegram bot setup with aiogram
-bot_token = "YOUR_BOT_TOKEN"
+bot_token = "6450878640:AAEkDXKORJvv-530GfG6OZYnZxfZgJ9f_FA"
 bot = Bot(token=bot_token, parse_mode="Markdown")  # Use "Markdown" instead of ParseMode.MARKDOWN
 
 # Initialize Dispatcher with bot
